bd583f9969d63887b848d233b069b0a95191d88e
ducanhaovai <ducanhaovai@users.noreply.github.com>
2024-01-10 23:03:29 +0700

    React
    Rol và Col da bam vao được

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,27 @@
-const initiaGameBoard = [
+import { useState } from "react";
+const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
 export default function GameBord() {
+    const [GameBoard, setGamBoard]= useState(initialGameBoard);
+    function handleSelectSquare(rowIndex, colIndex){
+        setGamBoard((prevGameBoard) =>{
+            const updatedBoard =[...prevGameBoard.map(innerArray=>[...innerArray])];
+            updatedBoard[rowIndex] [colIndex] ='X';
+           return updatedBoard;
+        })
+    }
     return (
         <ol id="game-board">
-            {initiaGameBoard.map((row, rowIndex) => (
+            {GameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) =>(
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={()=> handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
                             </li>
                             
                         ))}
